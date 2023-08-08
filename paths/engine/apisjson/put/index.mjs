747b7944c5f5454d8cc68812913d1dc1c226a896
Kin Lane <kinlane@gmail.com>
2023-08-08 17:14:15 -0400

    debug

--- paths/engine/apisjson/put/index.mjs
+++ paths/engine/apisjson/put/index.mjs
@@ -14,8 +14,6 @@ export function handler(event, context, callback) {
     });
     
     var sql = "SELECT url FROM openapi WHERE pulled IS NULL LIMIT 1";
-    connection.query(sql, function (error, results, fields) { 
-      
-    });  
+    callback(null,sql);
   
 };
