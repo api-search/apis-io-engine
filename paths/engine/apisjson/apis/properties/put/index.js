7bbec66439f79888ddb10817f95fccf75931a23d
Kin Lane <kinlane@gmail.com>
2023-08-09 16:04:57 -0400

    update latest

--- paths/engine/apisjson/apis/properties/put/index.js
+++ paths/engine/apisjson/apis/properties/put/index.js
@@ -31,135 +31,141 @@ exports.handler = vandium.generic()
     connection.query(sql, function (error, results, fields) { 
       
         if(results && results.length > 0){
-          
-        // Pull any new ones.
-        var api_base_url = results[0].api_base_url;
-        console.log(api_base_url);
-        var property_type = results[0].type;
-        console.log(property_type);
-        var property_url = results[0].url;
-        console.log(property_url);
-
-        var property_slug = property_url.replace('http://','http-');
-        property_slug = property_slug.replace('.json','');
-        property_slug = property_slug.replace('.yaml','');
-        property_slug = property_slug.replace('https://','https-');
-        property_slug = property_slug.replace(/\//g, '-');
-        property_slug = property_slug.replace('.','-');
-
-        var save_property_path = 'apis-io/api/apis-json/properties/' + property_slug + "/" + weekNumber + "/apis.json";
-
-          https.get(property_url, (res)=>{
             
-            let data = [];
-            const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-            
-            console.log('Status Code:', res.statusCode);
-            console.log('Date in Response header:', headerDate);
-          
-            res.on('data', chunk => {
-              data.push(chunk);
-            });
-          
-            res.on('end', () => {
+          // Pull any new ones.
+          var api_base_url = results[0].api_base_url;
+          //console.log(api_base_url);
+          var property_type = results[0].type;
+          //console.log(property_type);
+          var property_url = results[0].url;
+          //console.log(property_url);
+
+          var property_slug = property_url.replace('http://','http-');
+          property_slug = property_slug.replace('.json','');
+          property_slug = property_slug.replace('.yaml','');
+          property_slug = property_slug.replace('https://','https-');
+          property_slug = property_slug.replace(/\//g, '-');
+          property_slug = property_slug.replace('.','-');
+
+          var save_property_path = 'apis-io/api/apis-json/properties/' + property_slug + "/" + weekNumber + "/apis.json";
+
+            https.get(property_url, (res)=>{
               
-              console.log('Response ended: ');
-
-              if(!error && res.statusCode == 200){
+              let data = [];
+              const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+              
+              //console.log('Status Code:', res.statusCode);
+              //console.log('Date in Response header:', headerDate);
+            
+              res.on('data', chunk => {
+                data.push(chunk);
+              });
+            
+              res.on('end', () => {
                 
-                // Success - Record Run
-                const property_content = Buffer.concat(data).toString();
+                //console.log('Response ended: ');
 
-                var outcome = {};
-                outcome.status = res.statusCode;
-                outcome.property_content = property_content;
-                outcome.save_property_path = save_property_path;
+                if(!error && res.statusCode == 200){
+                  
+                  // Success - Record Run
+                  const property_content = Buffer.concat(data).toString();
 
-                var sql = "UPDATE properties SET pulled = " + connection.escape(weekNumber) + ",status = " + connection.escape(res.statusCode) + ",path = " + connection.escape(save_property_path) + " WHERE api_base_url = " + connection.escape(api_base_url) + " AND url = " + connection.escape(property_url);
-                outcome.sql = sql;
-                connection.query(sql, function (error, results, fields) {                  
+                  var outcome = {};
+                  outcome.status = res.statusCode;
+                  outcome.property_content = property_content;
+                  outcome.save_property_path = save_property_path;
 
-                  var sql1 = "DELETE FROM properties_pull WHERE url = '" + property_url + "' AND week = " + weekNumber;
-                  outcome.sql1 = sql1;
-                  connection.query(sql1, function (error1, results1, fields) { 
-                    
-                    var sql2 = "INSERT INTO properties_pull(pulled,url,status) VALUES(" + connection.escape(weekNumber) + "," + connection.escape(property_url) + "," + connection.escape(res.statusCode) + ")";
-                    outcome.sql2 = sql2;
-                    connection.query(sql2, function (error2, results2, fields) {     
+                  var sql = "UPDATE properties SET pulled = " + connection.escape(weekNumber) + ",status = " + connection.escape(res.statusCode) + ",path = " + connection.escape(save_property_path) + " WHERE api_base_url = " + connection.escape(api_base_url) + " AND url = " + connection.escape(property_url);
+                  outcome.sql = sql;
+                  connection.query(sql, function (error, results, fields) {                  
 
-                      // Write to S#
-                      var buf = Buffer.from(property_content);
+                    var sql1 = "DELETE FROM properties_pull WHERE url = '" + property_url + "' AND week = " + weekNumber;
+                    outcome.sql1 = sql1;
+                    connection.query(sql1, function (error1, results1, fields) { 
+                      
+                      var sql2 = "INSERT INTO properties_pull(pulled,url,status) VALUES(" + connection.escape(weekNumber) + "," + connection.escape(property_url) + "," + connection.escape(res.statusCode) + ")";
+                      outcome.sql2 = sql2;
+                      connection.query(sql2, function (error2, results2, fields) {     
 
-                      var params = {
-                          Bucket: 'kinlane-productions2',
-                          Key: save_property_path,
-                          Body: buf,
-                          ContentEncoding: 'base64',
-                          ContentType: 'application/json',
-                          ACL: 'public-read'
-                      };
+                        // Write to S#
+                        var buf = Buffer.from(property_content);
 
-                      console.log(params);  
+                        var params = {
+                            Bucket: 'kinlane-productions2',
+                            Key: save_property_path,
+                            Body: buf,
+                            ContentEncoding: 'base64',
+                            ContentType: 'application/json',
+                            ACL: 'public-read'
+                        };
 
-                      s3.putObject(params, function (err, data) {
-                          if (err) {
-                              reject(err)
-                          } else {
-                                
-                                console.log("Successfully uploaded data to bucket");
-                                
-                                outcome.data = data;
+                        //console.log(params);  
 
-                                outcome = {};
-                                outcome.message = "Successfully processed the API property."
+                        s3.putObject(params, function (err, data) {
+                            if (err) {
+                                reject(err)
+                            } else {
+                                  
+                                  //console.log("Successfully uploaded data to bucket");
+                                  
+                                  outcome.data = data;
 
-                                callback( null, outcome );     
-                          }
-                      });                                  
-                                        
+                                  outcome = {};
+                                  outcome.message = "Successfully processed the API property."
+
+                                  callback( null, outcome );     
+                            }
+                        });                                  
+                                          
 
 
-                    });
+                      });
+                    });   
+
+                  });
+
+                }
+                else{
+
+                  // Problem -- Record Run
+                  var outcome = {};
+                  outcome.status = res.statusCode;
+                  outcome.property_content = '';
+                  outcome.save_property_path = '';
+
+                  var sql = "UPDATE properties SET pulled = " + connection.escape(weekNumber) + ",status = " + connection.escape(res.statusCode) + ",path = '' WHERE api_base_url = " + connection.escape(api_base_url) + " AND url = " + connection.escape(property_url);
+                  outcome.sql = sql;
+                  connection.query(sql, function (error, results, fields) {                  
+
+                    var sql1 = "DELETE FROM properties_pull WHERE url = '" + property_url + "' AND week = " + weekNumber;
+                    outcome.sql1 = sql1;
+                    connection.query(sql1, function (error1, results1, fields) { 
+                      
+                      var sql2 = "INSERT INTO properties_pull(pulled,url,status) VALUES(" + connection.escape(weekNumber) + "," + connection.escape(property_url) + "," + connection.escape(res.statusCode) + ")";
+                      outcome.sql2 = sql2;
+                      connection.query(sql2, function (error2, results2, fields) {     
+
+                        outcome = {};
+                        outcome.message = "Status - " + res.statusCode;
+
+                        callback( null, outcome );
+
+                      });
+                    }); 
+
                   });   
 
-                });
+                }
 
-              }
-              else{
-
-                // Problem -- Record Run
-                var outcome = {};
-                outcome.status = res.statusCode;
-                outcome.property_content = '';
-                outcome.save_property_path = '';
-
-                var sql = "UPDATE properties SET pulled = " + connection.escape(weekNumber) + ",status = " + connection.escape(res.statusCode) + ",path = '' WHERE api_base_url = " + connection.escape(api_base_url) + " AND url = " + connection.escape(property_url);
-                outcome.sql = sql;
-                connection.query(sql, function (error, results, fields) {                  
-
-                  var sql1 = "DELETE FROM properties_pull WHERE url = '" + property_url + "' AND week = " + weekNumber;
-                  outcome.sql1 = sql1;
-                  connection.query(sql1, function (error1, results1, fields) { 
-                    
-                    var sql2 = "INSERT INTO properties_pull(pulled,url,status) VALUES(" + connection.escape(weekNumber) + "," + connection.escape(property_url) + "," + connection.escape(res.statusCode) + ")";
-                    outcome.sql2 = sql2;
-                    connection.query(sql2, function (error2, results2, fields) {     
-
-                      callback( null, outcome );
-
-                    });
-                  }); 
-
-                });   
-
-              }
+              });
 
             });
-
-          });
 
         }
         else{
+
+          outcome = {};
+          outcome.message = "No more properties to pull.";
 
           callback( null, error );
 
