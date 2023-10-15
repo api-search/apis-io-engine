1d734c7eee18d85e4d97d4cf91756f1acab51caf
Kin Lane <kinlane@gmail.com>
2023-10-14 20:03:51 -0400

    add overlay get

---
+++ paths/engine/apisjson/apis/overlay/get/index.js
@@ -0,0 +1,18 @@
+const mysql  = require('mysql');
+
+exports.handler = vandium.generic()
+  .handler( (event, context, callback) => {
+
+    var connection = mysql.createConnection({
+    host     : process.env.host,
+    user     : process.env.user,
+    password : process.env.password,
+    database : process.env.database
+    });
+
+    var sql = "SELECT name,slug,description,image FROM APIS ORDER BY name";
+
+    connection.query(sql, function (error, results, fields) {                                
+      callback( null, results );     
+    });      
+});
