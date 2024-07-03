8af3648be14239b2c9d260dd922cafe2a20712be
Kin Lane <info@apireview.shop>
2024-07-03 14:52:38 -0400

    debug

--- paths/engine/apisjson/put/index.js
+++ paths/engine/apisjson/put/index.js
@@ -35,7 +35,7 @@ exports.handler = vandium.generic()
     var provider_insert = "INSERT INTO providers(aid,name,description,tags,score,apis_json_url,search_node_url) VALUES(" + connection.escape(event.aid) + "," + connection.escape(event.name) + "," + connection.escape(event.description) + "," + connection.escape(event.tags.join(", ")) + "," + connection.escape(event.score) + "," + connection.escape(apis_json_url) + "," + connection.escape(search_node_url) + ")";
 
     // APIS
-    var api_insert = "INSERT INTO apis(aid,name,description,tags,score,apis_json_url,search_node_url) VALUES";
+    var api_insert = "INSERT INTO apis(aid,name,description,tags,score,apis_json_url,search_node_url,human_url) VALUES";
     for (let i = 0; i < event.apis.length; i++) {
       var human_url = event.apis[i].humanUrl;
       api_insert += "(" + connection.escape(event.apis[i].aid) + "," + connection.escape(event.apis[i].name) + "," + connection.escape(event.apis[i].description) + "," + connection.escape(event.apis[i].tags.join(", ")) + "," + connection.escape(event.apis[i].score) + "," + connection.escape(apis_json_url) + "," + connection.escape(search_node_url) + "," + connection.escape(human_url) + "),";
